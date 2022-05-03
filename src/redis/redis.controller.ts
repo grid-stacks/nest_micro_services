5550348690027d2094d6fe26152c2de6b5599cd7
DHN Chandan <php.chandan@gmail.com>
2022-05-03 21:27:27 +0600

    Test om redis successfully

--- src/redis/redis.controller.ts
+++ src/redis/redis.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { RedisService } from './redis.service';
 
 @Controller('redis')
 export class RedisController {
   constructor(private readonly redisService: RedisService) {}
+
+  @Get('om_test')
+  async omRedisTest() {
+    return await this.redisService.omRedisTest();
+  }
 }
--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import Redis from 'ioredis';
+import { Client } from 'redis-om';
+import { IORedisKey } from './ioredis.config.module';
+import { OMRedisKey } from './omredis.config.module';
 
 @Injectable()
-export class RedisService {}
+export class RedisService {
+  constructor(
+    @Inject(IORedisKey) private readonly ioRedisClient: Redis,
+    @Inject(OMRedisKey) private readonly omRedisClient: Client,
+  ) {}
+
+  async omRedisTest() {
+    //   const setFoo = await this.omRedisClient.set('foo', 'bar');
+    const getFoo = await this.omRedisClient.get('foo');
+
+    return getFoo;
+  }
+}
