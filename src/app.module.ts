cf1264b7e55e6a5742dd8c5bd5590bfbddb9c075
DHN Chandan <php.chandan@gmail.com>
2022-05-06 06:27:07 +0600

    Add search module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RedisModule } from './redis/redis.module';
+import { SearchModule } from './search/search.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), RedisModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    RedisModule,
+    SearchModule,
+  ],
 
   controllers: [AppController],
   providers: [AppService],
