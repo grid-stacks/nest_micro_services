3086a7d4be36e0880b79220e3030611f04892252
DHN Chandan <php.chandan@gmail.com>
2022-05-07 07:18:11 +0600

    Replace logger to pino

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from 'nestjs-pino';
+import { stdTimeFunctions } from 'pino';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RedisModule } from './redis/redis.module';
@@ -10,6 +12,20 @@ import { SearchModule } from './search/search.module';
     ConfigModule.forRoot({ isGlobal: true }),
     RedisModule,
     SearchModule,
+    LoggerModule.forRoot({
+      pinoHttp: {
+        timestamp: stdTimeFunctions.isoTime,
+        level: 'trace',
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            levelFirst: true,
+            translateTime: true,
+            colorize: true,
+          },
+        },
+      },
+    }),
   ],
 
   controllers: [AppController],
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,40 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { Logger } from 'nestjs-pino';
 import { AppModule } from './app.module';
 import tracer from './tracer';
+
+const signalsNames: NodeJS.Signals[] = ['SIGTERM', 'SIGINT', 'SIGHUP'];
 
 async function bootstrap() {
   await tracer.start();
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { logger: false });
+
+  const logger = app.get(Logger);
+
+  signalsNames.forEach((signalName) =>
+    process.on(signalName, (signal) => {
+      logger.log(`Retrieved signal: ${signal}, application terminated`);
+      process.exit(0);
+    }),
+  );
+
+  process.on('uncaughtException', (error: Error) => {
+    logger.error({ err: error });
+    process.exit(1);
+  });
+
+  process.on('unhandledRejection', (reason, promise) => {
+    logger.error(`Unhandled Promise Rejection, reason: ${reason}`);
+    promise.catch((err: Error) => {
+      logger.error({ err });
+      process.exit(1);
+    });
+  });
+
+  app.useLogger(logger);
+
   await app.listen(3000);
 }
 bootstrap();
